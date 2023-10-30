687d0165a235c7bdfd9d99edeaace5c7b9659c48
NonoIceOff <nonocraft28260@gmail.com>
2023-10-30 22:10:53 +0100

    story 2 début

--- ASSETS/scripts/memory_game.js
+++ ASSETS/scripts/memory_game.js
@@ -2,7 +2,8 @@ let numbersOfPairs = 0;
 let pairsGenerated = [];
 let gridCards = [];
 
-function generateGrid() { // Générer la grille
+function generateGrid() {
+  // Générer la grille
   let xCards = 5;
   let yCards = 5;
 
@@ -12,7 +13,8 @@ function generateGrid() { // Générer la grille
   let grid = document.getElementById("grid"); // définit les bordels
   let textDifficulty = document.getElementById("textDifficulty"); // définit les bordels
 
-  if (difficulty.value == "easy") { // Initialise les variables pour une difficulté choisie
+  if (difficulty.value == "easy") {
+    // Initialise les variables pour une difficulté choisie
     xCards = 6;
     yCards = 5;
     numbersOfPairs = 15;
@@ -29,7 +31,8 @@ function generateGrid() { // Générer la grille
     textDifficulty.textContent = "difficile";
   }
 
-  if (themes.value == "classic") { // Définit le dossier des images à l'aide de la difficulté choisie
+  if (themes.value == "classic") {
+    // Définit le dossier des images à l'aide de la difficulté choisie
     themeName = "classic";
   } else if (themes.value == "numbers") {
     themeName = "numbers";
@@ -37,7 +40,8 @@ function generateGrid() { // Générer la grille
     themeName = "politics";
   }
 
-  for (i = 0; i < numbersOfPairs; i++) { // Génère l'array du compte des pairs générés
+  for (i = 0; i < numbersOfPairs; i++) {
+    // Génère l'array du compte des pairs générés
     pairsGenerated.push(0);
   }
 
@@ -49,17 +53,25 @@ function generateGrid() { // Générer la grille
     }
   }
 
-  for (i = 0; i < xCards; i++) { // Création et affichage des images
+  for (i = 0; i < xCards; i++) {
+    // Création et affichage des images
     let div = document.createElement("div");
     grid.append(div);
     for (j = 0; j < yCards; j++) {
       let buttonImage = document.createElement("button");
+      buttonImage.setAttribute("data-i", i);
+      buttonImage.setAttribute("data-j", j);
       let image = document.createElement("img");
       image.src =
         "../../assets/images/cards/" + themeName.toString() + "/dos_cartes.png"; // Ajoute la source à l'image
       buttonImage.append(image);
       div.append(buttonImage); // Ajoute une image
       setPair(i, j);
+
+      // Appel de revealCard pour révéler les cartes
+      buttonImage.addEventListener("click", function (event) {
+        revealCard(event, themeName);
+      });
     }
   }
 
@@ -69,29 +81,46 @@ function generateGrid() { // Générer la grille
   inGame(); // Lancement du jeu
 }
 
-function setPair(i, j) { // Définit et attribue au carte des pairs
+function revealCard(event, themeName) {
+  let buttonImage = event.currentTarget;
+  let i = buttonImage.getAttribute("data-i");
+  let j = buttonImage.getAttribute("data-j");
+
+  let image = document.createElement("img");
+  let pairNumber = gridCards[i][j];
+  image.src = `../../assets/images/cards/${themeName}/${pairNumber}.png`;
+  buttonImage.innerHTML = ''; // Efface le contenu actuel du bouton
+  buttonImage.append(image);
+
+  // note pour demain: la logique de la comparaison etc....
+}
+
+
+
+function setPair(i, j) {
+  // Définit et attribue au carte des pairs
   var pairok = false;
   while (pairok === false) {
     var pairNumber = Math.floor(Math.random() * numbersOfPairs);
     if (pairsGenerated[pairNumber] < 2) {
       pairsGenerated[pairNumber]++;
-      gridCards[i][j] = pairNumber;
+      gridCards[i][j] = pairNumber+1;
       pairok = true;
     } else {
-        pairok = false;
+      pairok = false;
     }
   }
 }
 
-function inGame() { // Fonction du jeu
+function inGame() {
+  // Fonction du jeu
   let secondes = 0;
   let textTimer = document.getElementById("textTimer"); // définit les bordels
   timer();
 }
 
-
 // TIMER //
-var h1 = document.getElementById('time');
+var h1 = document.getElementById("time");
 var sec = 0;
 var min = 0;
 var hrs = 0;
@@ -110,13 +139,14 @@ function tick() {
 }
 function add() {
   tick();
-  h1.textContent = (hrs > 9 ? hrs : '0' + hrs) + ':' +
-      (min > 9 ? min : '0' + min) + ':' + (sec > 9 ? sec : '0' + sec);
+  h1.textContent =
+    (hrs > 9 ? hrs : "0" + hrs) +
+    ":" +
+    (min > 9 ? min : "0" + min) +
+    ":" +
+    (sec > 9 ? sec : "0" + sec);
   timer();
 }
 function timer() {
   t = setTimeout(add, 1000);
 }
-
-
-
