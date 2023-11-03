4118eecc236a5afb1c19af504cda9f7e1b8aa911
NonoIceOff <nonocraft28260@gmail.com>
2023-11-03 11:15:27 +0100

    café

--- register.php
+++ register.php
@@ -48,7 +48,7 @@ if (isset($_POST['email'])) {
 	require SITE_ROOT . 'partials/head.php';
 	?>
 	<title>S'inscrire</title>
-	<script src="./ASSETS/scripts/register.js"></script>
+	<script src="./ASSETS/script/register.js"></script>
 </head>
 
 <body>
@@ -97,4 +97,25 @@ if (isset($_POST['email'])) {
 	</header>
 	<!------------------------------------------------------------->
 
+	<footer>
+	<div class="container">
+    <div class="coffee-header">
+      <div class="coffee-header__buttons coffee-header__button-one"></div>
+      <div class="coffee-header__buttons coffee-header__button-two"></div>
+      <div class="coffee-header__display"></div>
+      <div class="coffee-header__details"></div>
+    </div>
+    <div class="coffee-medium">
+      <div class="coffe-medium__exit"></div>
+      <div class="coffee-medium__arm"></div>
+      <div class="coffee-medium__liquid"></div>
+      <div class="coffee-medium__smoke coffee-medium__smoke-one"></div>
+      <div class="coffee-medium__smoke coffee-medium__smoke-two"></div>
+      <div class="coffee-medium__smoke coffee-medium__smoke-three"></div>
+      <div class="coffee-medium__smoke coffee-medium__smoke-for"></div>
+      <div class="coffee-medium__cup"></div>
+    </div>
+    <div class="coffee-footer"></div>
+  </div>
+	</footer>
 </body>
